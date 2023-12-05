7a9f3ec686fdb48dd0789f0328cd9822f9811eda
nguyenhuy112 <101613002+nguyenhuy112@users.noreply.github.com>
2023-12-05 09:13:06 +0700

    BTTH6.cpp

--- BTTH6.cpp
+++ BTTH6.cpp
@@ -1,61 +1,50 @@
 #include <stdio.h>
-#define MAX_SIZE 100
+#include <stdlib.h>
 
-void nhapMang(int *arr, int size) {
-    printf("Nhap cac phan tu cua mang:\n");
-    for (int i = 0; i < size; i++) {
-        printf("arr[%d] = ", i);
-        scanf("%d", (arr + i));
-    }
+struct Book {
+    char ten[50];
+    char tacGia[50];
+    float gia;
+};
+
+void nhapThongTinSach(struct Book *sach) {
+    getchar();
+    printf("Nhap ten sach: ");
+    fgets(sach->ten, 50, stdin);
+    printf("Nhap tac gia: ");
+    fgets(sach->tacGia, 50, stdin);
+    printf("Nhap gia sach: ");
+    scanf("%f", &sach->gia);
 }
-
-void inMang(const int *arr, int size) {
-    printf("Cac phan tu cua mang la:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
-    }
-    printf("\n");
-}
-
-
-int timKiem(const int *arr, int size, int key) {
-    for (int i = 0; i < size; i++) {
-        if (*(arr + i) == key) {
-            return i; 
-        }
-    }
-    return -1; 
+void hienThiThongTinSach(struct Book *sach) {
+    printf("Ten sach: %s\n", sach->ten);
+    printf("Tac gia: %s\n", sach->tacGia);
+    printf("Gia sach: %.2f\n", sach->gia);
 }
 
 int main() {
-    int arr[MAX_SIZE];
-    int size, key, result;
+    int n; 
+    printf("Nhap so luong sach: ");
+    scanf("%d", &n);
+    struct Book *dsSach = (struct Book *)malloc(n * sizeof(struct Book));
 
-    printf("Nhap kich thuoc cua mang: ");
-    scanf("%d", &size);
-
-    if (size <= 0 || size > MAX_SIZE) {
-        printf("Kich thuoc khong hop le. Vui long nhap lai.\n");
+    if (dsSach == NULL) {
+        printf("Loi! Khong the cap phat bo nho.");
         return 1; 
     }
 
-    
-    nhapMang(arr, size);
-
-    inMang(arr, size);
-
-    
-    printf("Nhap gia tri can tim kiem: ");
-    scanf("%d", &key);
-
-    result = timKiem(arr, size, key);
-
-    if (result != -1) {
-        printf("Phan tu %d duoc tim thay tai vi tri %d.\n", key, result);
-    } else {
-        printf("Phan tu %d khong tim thay trong mang.\n", key);
+    for (int i = 0; i < n; i++) {
+        printf("\nNhap thong tin cho sach %d:\n", i + 1);
+        
+        nhapThongTinSach(&dsSach[i]);
     }
+	printf ("Danh sach sach da nhap:\n");
+    for (int i = 0; i < n; i++) {	
+    printf("\nThong tin sach %d:\n", i + 1);
+        hienThiThongTinSach(&dsSach[i]);
+    }
+    free(dsSach);
 
-    return 0;
+    return 0; 
 }
 
